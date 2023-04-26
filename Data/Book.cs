a258cf10e5f22562f82b9b9f9eeead47d8938a7a
Zoë Goodwin <zgoodwin@gmail.com>
2023-04-26 12:54:39 -0600

    CSS and Classes
    Added 2 interface classes (searchable, loanable), 1 abstract class (book), and library, loanablebook:book, referencebook:book. no classes are complete, just fleshing out

---
+++ Data/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotech0.Data
+{
+	public abstract class Book
+	{
+		private string title;
+		private string authorFirstName;
+		private string authorLastName;
+		private int isbn;
+		private string location;
+
+		//class constructor
+		public Book(string title, string authorFirstName, string authorLastName, int isbn, string location)
+		{
+			this.title = title;
+			this.authorFirstName = authorFirstName;
+			this.authorLastName = authorLastName;
+			this.isbn = isbn;
+			this.location = location;
+		}
+
+		//getters & setters
+		public string Title { get => title; set => title = value; }
+		public string AuthorFirstName { get => authorFirstName; set => authorFirstName = value; }
+		public string AuthorLastName { get => authorLastName; set => authorLastName = value; }
+		public int Isbn { get => isbn; set => isbn = value; }
+		public string Location { get => location; set => location = value; }
+
+	}
+}
---
+++ Data/Searchable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotech0.Data
+{
+	public interface Searchable
+	{
+		// method - search for objects (users or books) based on the user's/staff's input
+		List<object> Search(string keyword);
+	}
+}
