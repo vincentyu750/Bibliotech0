203bc7c125a9584a6ffad2382104f5bef693e578
Zoë Goodwin <zgoodwin@gmail.com>
2023-04-28 17:15:25 -0600

    Class Additions

---
+++ Data/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotech0.Data
+{
+	public class Book
+	{
+		//[PrimaryKey, AutoIncrement]
+		public int ID { get; set; }
+		public string Title { get; set; }
+		public string AuthorFirstName { get; set; }
+		public string AuthorLastName { get; set; }
+		public long Isbn { get; set; }
+		public string Location { get; set; }
+
+		public Book() { }
+
+		public Book(int id, string title, string authorFirstName, string authorLastName, long isbn, string location)
+		{
+			ID = id;
+			Title = title;
+			AuthorFirstName = authorFirstName;
+			AuthorLastName = authorLastName;
+			Isbn = isbn;
+			Location = location;
+		}
+	}
+
+}
---
+++ Data/Cardholder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotech0.Data
+{
+	public class Cardholder : User
+	{
+		private int cardNumber;
+		private double balance;
+		private bool itemsOnLoan;
+
+		//class constructor
+		public Cardholder(string firstName, string lastName, string email, string password, string phone, int cardNumber, double balance, bool itemsOnLoan) : base(firstName, lastName, email, password, phone)
+		{
+			this.cardNumber = cardNumber;
+			this.balance = balance;
+			this.itemsOnLoan = itemsOnLoan;
+		}
+
+		//getters & setters
+		public int CardNumber { get => cardNumber; set => cardNumber = value; }
+		public double Balance { get => balance; set => balance = value; }
+		public bool ItemsOnLoan { get => itemsOnLoan; set => itemsOnLoan = value; }
+
+	}
+}
