5b5b6c994734bfc885c08873d8496c8b0de3725f
Zoë Goodwin <zgoodwin@gmail.com>
2023-04-28 17:17:59 -0600

    Update to Staff class
    now an instance of User abstract class

---
+++ Data/Staff.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliotech0.Data
+{
+	public class Staff : User
+	{
+		private int staffID;
+
+		//class constructor
+		public Staff(string firstName, string lastName, string email, string password, string phone, int staffID) : base(firstName, lastName, email, password, phone)
+		{
+			this.staffID = staffID;
+		}
+
+		//getters & setters
+		public int StaffID { get => staffID; set => staffID = value; }
+
+	}
+}
