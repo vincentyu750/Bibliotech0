37398a5d17d77479d2210e704575513bda9677b0
Vincent Yu <70932487+vincentyu750@users.noreply.github.com>
2023-04-29 00:09:25 -0600

    Services
    Services

---
+++ Services/BookService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bibliotech0.Database;
+using Bibliotech0.Data;
+using Bibliotech0.Services;
+using Bibliotech0.Interface;
+
+namespace Bibliotech0.Services
+{
+	public class BookService : IBookService
+	{
+		public List<LoanableBook> BorrowedBooks { get; set; }
+
+		public BookService()
+		{
+			BorrowedBooks = new List<LoanableBook>();
+		}
+	}
+}
---
+++ Services/FindBookService.cs
@@ -0,0 +1,46 @@
+﻿using SQLite;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Bibliotech0.Data;
+using Microsoft.Data.Sqlite;
+
+
+namespace Bibliotech0.Services
+{
+    public class FindBookService
+    {
+        static SQLiteAsyncConnection db;
+        static string databasePath = Path.Combine(new DirectoryInfo(AppContext.BaseDirectory).Parent.Parent.Parent.Parent.Parent.FullName, "Database", "LibraryDatabase.db");
+
+        static async Task Init()
+        {
+
+            db = new SQLiteAsyncConnection(databasePath);
+
+            await db.CreateTableAsync<Book>();
+        }
+
+        public static async Task<List<Book>> GetBooks()
+        {
+            await Init();
+            List<Book> books = await db.Table<Book>().Where(b => b.ID > 0).ToListAsync();
+            return books;
+        }
+
+        public static async Task AddBook(Book book)
+        {
+            await Init();
+            await db.InsertAsync(book);
+        }
+
+        public static async Task RemoveBook(int isbn)
+        {
+            await Init();
+            await db.DeleteAsync<Book>(isbn);
+        }
+
+
+    }
+}
+
