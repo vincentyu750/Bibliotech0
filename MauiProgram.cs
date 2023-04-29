5b0653ecf9a1a222af36e8cca94825fdb7f32522
Vincent Yu <70932487+vincentyu750@users.noreply.github.com>
2023-04-29 00:11:31 -0600

    Adding Remaining Files
    Adding Remaining Files

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,29 +1,43 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
 using Bibliotech0.Data;
+using Bibliotech0.Services;
+using Bibliotech0.Database;
+using Bibliotech0.Interface;
+using SQLite;
+using System.IO;
+using Microsoft.Extensions.Logging;
 
-namespace Bibliotech0;
-
-public static class MauiProgram
+namespace Bibliotech0
 {
-	public static MauiApp CreateMauiApp()
+	public static class MauiProgram
 	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+		public static MauiApp CreateMauiApp()
+		{
+			var builder = MauiApp.CreateBuilder();
+			builder
+				.UseMauiApp<App>()
+				.ConfigureFonts(fonts =>
+				{
+					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				});
 
-		builder.Services.AddMauiBlazorWebView();
-
+			builder.Services.AddBlazorWebView();
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
-		builder.Logging.AddDebug();
+			builder.Services.AddBlazorWebViewDeveloperTools();
+			builder.Logging.AddDebug();
+			builder.Services.AddSingleton<FindBookService>();
 #endif
+			// Add the path to your SQLite database here
+			var databasePath = Path.Combine(new DirectoryInfo(AppContext.BaseDirectory).Parent.Parent.Parent.Parent.Parent.FullName, "Database", "LibraryDatabase.db");
+			var db = new SQLiteAsyncConnection(databasePath);
+			builder.Services.AddSingleton(db);
 
-		//builder.Services.AddSingleton<WeatherForecastService>();
-
-		return builder.Build();
+			builder.Services.AddMauiBlazorWebView();
+			builder.Services.AddSingleton<IBookService, BookService>();
+			return builder.Build();
+		}
 	}
 }
---
+++ Services/CardService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bibliotech0.Data;
+using Bibliotech0.Interface;
+
+namespace Bibliotech0.Services
+{
+	public class CardService : ICardService
+	{
+		public List<Cardholder> CardHolders { get; set; }
+        public Cardholder CurrentCardHolder { get; set; }
+
+        public CardService()
+		{
+            CardHolders = new List<Cardholder>
+            {
+                new Cardholder("Vincent", "Yu", "vincent.yu@sait.com", "123", "555-555-5555", 123456789, 50.0, false),
+                new Cardholder("John", "Doe", "johndoe@example.com", "password123", "555-555-5555", 123456789, 50.0, false)
+            };
+        }
+
+    }
+}
